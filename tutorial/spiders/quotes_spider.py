72df46e3ecd16c146e868435eecb5e7c02982e8b
rodionIlnitskey <rodion9181@gmail.com>
2021-10-12 20:45:07 +0300

    Scrapy project

---
+++ tutorial/spiders/quotes_spider.py
@@ -0,0 +1,19 @@
+import scrapy
+
+
+class QuotesSpider(scrapy.Spider):
+    name = "quotes"
+    start_urls = [
+        'http://quotes.toscrape.com/page/1/',
+    ]
+
+    def parse(self, response):
+        for quote in response.css('span.tag-item'):
+            yield {
+                'text': quote.xpath('a/text()').getall(),
+            }
+
+        next_page = response.css('li.next a::attr(href)').get()
+        if next_page is not None:
+            next_page = response.urljoin(next_page)
+            yield scrapy.Request(next_page, callback=self.parse)
