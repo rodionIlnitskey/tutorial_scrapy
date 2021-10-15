8c63a8eb51b3ec6759ece8b7307d0e0deb8fa822
rodionIlnitskey <rodion9181@gmail.com>
2021-10-15 17:54:54 +0300

    Now you can scrape author and information from their pages

--- tutorial/spiders/quotes_spider.py
+++ tutorial/spiders/quotes_spider.py
@@ -4,16 +4,27 @@ import scrapy
 class QuotesSpider(scrapy.Spider):
     name = "quotes"
     start_urls = [
-        'http://quotes.toscrape.com/page/1/',
+        'http://quotes.toscrape.com/',
     ]
 
     def parse(self, response):
-        for quote in response.css('span.tag-item'):
+        for quote in response.css('div.quote'):
             yield {
-                'text': quote.xpath('a/text()').getall(),
+                'text': quote.css('small.author::text').getall(),
             }
 
         next_page = response.css('li.next a::attr(href)').get()
         if next_page is not None:
             next_page = response.urljoin(next_page)
             yield scrapy.Request(next_page, callback=self.parse)
+
+
+
+        for cn in response.css('head'):
+            yield {
+                'ds': cn.xpath('title').getall(),
+            }
+
+
+        for a in response.css('div.quote span a'):
+            yield response.follow(a, callback=self.parse)
